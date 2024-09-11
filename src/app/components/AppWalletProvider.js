0fb09f91cf647a676df602c5288d846d34172c2c
mani-rive <mani@rive.com>
2024-09-11 20:06:23 +0530

    added the wallet-adapter

---
+++ src/app/components/AppWalletProvider.js
@@ -0,0 +1,25 @@
+"use client"; // Ensure this component runs only on the client
+
+import React from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+
+// Solana wallet adapter UI styles
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+export default function AppWalletProvider({ children }) {
+  const network = WalletAdapterNetwork.Devnet;
+
+  return (
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
